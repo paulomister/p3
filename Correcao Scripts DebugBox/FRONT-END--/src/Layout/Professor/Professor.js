9b2efc0b6f8850ff456b8db2d15eafdd12f0c00d
yhagou <151045107+yhagou@users.noreply.github.com>
2025-02-21 17:04:21 -0300

    "Código comentado e Diagrama de sequencia"

---
+++ Correcao Scripts DebugBox/FRONT-END--/src/Layout/Professor/Professor.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Componente responsável pela página do professor
+export default function Professor() {
+  return (
+    <div>
+      {/* Conteúdo da página do professor será adicionado aqui futuramente */}
+    </div>
+  );
+}
